b0496c727e2b9eefb2bb016ccbb6f74e9656cb38
Jim Martens <github@2martens.de>
2026-01-10 11:57:39 +0100

    fix: Import user as type

--- src/access/anyone.ts
+++ src/access/anyone.ts
@@ -1,3 +1,5 @@
-import type { AccessArgs, User } from 'payload'
+import type { AccessArgs } from 'payload'
+
+import type { User } from '@/payload-types'
 
 export const anyone: (args: AccessArgs<User>) => boolean = () => true
